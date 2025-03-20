c2739d78b259d702514d18b2e9eca72ccc434c84
PedroRomaoDev <pedro7ntj@gmail.com>
2025-03-20 12:57:15 -0300

    ok vini

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // RAIZ DO PROJETO
+import 'dotenv/config.js';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+// eslint-disable-next-line no-undef
+app.listen(process.env.PORT, () =>
+    // eslint-disable-next-line no-undef
+    console.log(`listening on port ${process.env.PORT}`),
+);
