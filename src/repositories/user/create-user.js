56363ee7ae685a3901715373218a3269ea40954a
PedroRomaoDev <pedro7ntj@gmail.com>
2025-03-25 15:04:43 -0300

    add PostgresCreateUserRepository

---
+++ src/repositories/user/create-user.js
@@ -0,0 +1,13 @@
+import User from '../../models/user.js';
+
+export class PostgresCreateUserRepository {
+    async execute(createUserParams) {
+        const user = await User.create({
+            first_name: createUserParams.first_name,
+            last_name: createUserParams.last_name,
+            email: createUserParams.email,
+            password: createUserParams.password,
+        });
+        return user;
+    }
+}
