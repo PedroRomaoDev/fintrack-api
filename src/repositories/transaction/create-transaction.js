24c80d29d6c9a6d9c72c358581bbc2ced7bf60f2
PedroRomaoDev <pedro7ntj@gmail.com>
2025-03-25 15:10:41 -0300

    add PostgresCreateTransactionRepository

---
+++ src/repositories/transaction/create-transaction.js
@@ -0,0 +1,14 @@
+import OfxTransacao from '../../models/ofxTransaction.js';
+
+export class PostgreCreateTransactionRepository {
+    async execute(createTransactionParams) {
+        return await OfxTransacao.create({
+            user_id: createTransactionParams.user_id,
+            tipo: createTransactionParams.tipo,
+            valor: createTransactionParams.valor,
+            data: createTransactionParams.data || new Date(),
+            descricao: createTransactionParams.descricao,
+            ofx_imported: createTransactionParams.ofx_imported ?? false,
+        });
+    }
+}
