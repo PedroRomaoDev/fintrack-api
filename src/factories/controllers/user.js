aa37f8a7a6baf73b9319ffe4c3d7b94b7d0c2dbd
PedroRomaoDev <pedro7ntj@gmail.com>
2025-03-25 16:20:18 -0300

    add get user by id factory

--- src/factories/controllers/user.js
+++ src/factories/controllers/user.js
@@ -1,9 +1,26 @@
 import {
+    PostgresGetUserByIdRepository,
     PostgresCreateUserRepository,
     PostgresGetUserByEmailRepository,
 } from '../../repositories/postgres/index.js';
-import { CreateUserUseCase } from '../../use-cases/index.js';
-import { CreateUserController } from '../../controllers/index.js';
+import {
+    GetUserByIdUseCase,
+    CreateUserUseCase,
+} from '../../use-cases/index.js';
+import {
+    GetUserByIdController,
+    CreateUserController,
+} from '../../controllers/index.js';
+
+export const makeGetUserByIdController = () => {
+    const getUserByIdRepository = new PostgresGetUserByIdRepository();
+
+    const getUserByIdUseCase = new GetUserByIdUseCase(getUserByIdRepository);
+
+    const getUserByIdController = new GetUserByIdController(getUserByIdUseCase);
+
+    return getUserByIdController;
+};
 
 export const makeCreateUserController = () => {
     const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
