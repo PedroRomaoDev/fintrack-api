68b2d0506633d7ad1da3fa4224de2adba4b86549
PedroRomaoDev <pedro7ntj@gmail.com>
2025-03-31 15:32:20 -0300

    feat: add GetUserByEmailRepositoryPostgres

---
+++ src/repositories/postgres/user/get-user-by-email.js
@@ -0,0 +1,11 @@
+import { prisma } from '../../../../prisma/prisma.js';
+
+export class PostgresGetUserByEmailRepository {
+    async execute(email) {
+        return await prisma.user.findUnique({
+            where: {
+                email,
+            },
+        });
+    }
+}
