229a5caad32dab8b9439bc8dcf3d9aec68c02ad4
PedroRomaoDev <pedro7ntj@gmail.com>
2025-03-31 16:27:50 -0300

    feat: add PostgresDeleteUserRepository

---
+++ src/repositories/postgres/user/delete-user.js
@@ -0,0 +1,14 @@
+import { prisma } from '../../../../prisma/prisma.js';
+
+export class PostgresDeleteUserRepository {
+    async execute(userId) {
+        try {
+            return await prisma.user.delete({
+                where: { id: userId },
+            });
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
+}
