eab5634fa6dedd2fa411bca8b1f8a598be67449b
PedroRomaoDev <pedro7ntj@gmail.com>
2025-03-31 15:34:38 -0300

    feat: add CreateUser route to index.js with express

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 // RAIZ DO PROJETO
 import 'dotenv/config.js';
 import express from 'express';
-import sequelize from './config/db.js';
 import { makeCreateUserController } from './src/factories/controllers/user.js';
-//
-async function testarConexao() {
-    try {
-        await sequelize.authenticate(); // Adicionei o `await` aqui
-        console.log('Conexão estabelecida com banco de dados');
-    } catch (err) {
-        console.log('Erro ao conectar', err);
-    }
-}
 
-testarConexao();
+//
 
 const app = express();
 
 app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
 
 app.post('/api/users', async (request, response) => {
     const createUserController = makeCreateUserController();
