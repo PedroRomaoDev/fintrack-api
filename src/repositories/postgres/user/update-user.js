6eff19030a5c58277839a6422384f4d76f89f374
PedroRomaoDev <pedro7ntj@gmail.com>
2025-03-31 15:45:18 -0300

    feat: add PostgresUpdateUserRepository

---
+++ src/repositories/postgres/user/update-user.js
@@ -0,0 +1,12 @@
+import { prisma } from '../../../../prisma/prisma.js';
+
+export class PostgresUpdateUserRepository {
+    async execute(userId, updateUserParams) {
+        return await prisma.user.update({
+            where: {
+                id: userId,
+            },
+            data: updateUserParams,
+        });
+    }
+}
