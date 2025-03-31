7220e3ee9833282855b1572032dc1795a6ecbe5e
PedroRomaoDev <pedro7ntj@gmail.com>
2025-03-31 15:19:37 -0300

    feat: add CreateUserController

---
+++ src/controllers/user/create-user.js
@@ -0,0 +1,33 @@
+import { badRequest, created, serverError } from '../helpers/index.js';
+import { EmailAlreadyInUseError } from '../../../errors/user.js';
+import { ZodError } from 'zod';
+import { createUserSchema } from '../../../schemas/index.js';
+
+export class CreateUserController {
+    constructor(createUserUseCase) {
+        this.createUserUseCase = createUserUseCase;
+    }
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body;
+
+            await createUserSchema.parseAsync(params);
+
+            const createdUser = await this.createUserUseCase.execute(params);
+
+            return created(createdUser);
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                });
+            }
+            if (error instanceof EmailAlreadyInUseError) {
+                return badRequest({ message: error.message });
+            }
+
+            console.error(error);
+            return serverError();
+        }
+    }
+}
