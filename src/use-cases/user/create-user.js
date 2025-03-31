fbb28252f8e4912cc86d8811635278e9a40d44c0
PedroRomaoDev <pedro7ntj@gmail.com>
2025-03-31 15:16:49 -0300

    feat: add PostgresCreateUserRepository and CreateUserUseCase

---
+++ src/use-cases/user/create-user.js
@@ -0,0 +1,38 @@
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
+import { EmailAlreadyInUseError } from '../../../errors/user.js';
+
+export class CreateUserUseCase {
+    constructor(getUserByEmailRepository, createUserRepository) {
+        this.getUserByEmailRepository = getUserByEmailRepository;
+        this.createUserRepository = createUserRepository;
+    }
+    async execute(createUserParams) {
+        //verificar se o email já está em uso
+
+        const userWithProviderEmail =
+            await this.getUserByEmailRepository.execute(createUserParams.email);
+
+        if (userWithProviderEmail) {
+            throw new EmailAlreadyInUseError(createUserParams.email);
+        }
+
+        //gerar ID do usuario com UUID
+        const userId = uuidv4();
+
+        //criptografar a senha com Bcrypt
+        const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+
+        //inserir usuario no banco de dados
+        const user = {
+            ...createUserParams, //firstName, lastName, email...estão inclusos aqui
+            id: userId,
+            password: hashedPassword,
+        };
+        // chamar o repositorio
+
+        const createdUser = await this.createUserRepository.execute(user);
+
+        return createdUser;
+    }
+}
