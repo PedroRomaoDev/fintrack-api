77b439f087883568f67ad3977648d7780eb2586e
PedroRomaoDev <pedro7ntj@gmail.com>
2025-04-19 11:49:15 -0300

    feat: add PostgresGetTransactionByUserIdRepository

---
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.js
@@ -0,0 +1,10 @@
+import { prisma } from '../../../../prisma/prisma.js';
+export class PostgresGetTransactionByUserIdRepository {
+    async execute(userId) {
+        return await prisma.transaction.findMany({
+            where: {
+                user_id: userId,
+            },
+        });
+    }
+}
