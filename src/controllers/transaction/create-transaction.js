110338f22b6a905b15b13c8eefe4c8d5ca8d1a1f
PedroRomaoDev <pedro7ntj@gmail.com>
2025-04-19 11:58:10 -0300

    feat: add CreateTransactionController

---
+++ src/controllers/transaction/create-transaction.js
@@ -0,0 +1,29 @@
+import { serverError, created, badRequest } from '../helpers/index.js';
+import { createTransactionSchema } from '../../../schemas/index.js';
+import { ZodError } from 'zod';
+
+export class CreateTransactionController {
+    constructor(createTransactionUseCase) {
+        this.createTransactionUseCase = createTransactionUseCase;
+    }
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body;
+
+            await createTransactionSchema.parseAsync(params);
+
+            const transaction =
+                await this.createTransactionUseCase.execute(params);
+
+            return created(transaction);
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                });
+            }
+            console.error(error);
+            return serverError(); //retorna um erro 500
+        }
+    }
+}
