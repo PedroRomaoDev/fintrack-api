6f3603e49a646c9c94031072eb0b398391cc797e
PedroRomaoDev <pedro7ntj@gmail.com>
2025-04-19 13:29:09 -0300

    feat: add maekUpdateTransaction factory

--- src/factories/controllers/transaction.js
+++ src/factories/controllers/transaction.js
@@ -1,15 +1,18 @@
 import {
     CreateTransactionController,
     GetTransactionsByUserIdController,
+    UpdateTransactionController,
 } from '../../controllers/index.js';
 import {
     PostgresGetUserByIdRepository,
     PostgresCreateTransactionRepository,
     PostgresGetTransactionByUserIdRepository,
+    PostgresUpdateTransactionRepository,
 } from '../../repositories/postgres/index.js';
 import {
     CreateTransactionUseCase,
     GetTransactionsByUserIdUseCase,
+    UpdateTransactionUseCase,
 } from '../../use-cases/index.js';
 import { IdGeneratorAdapter } from '../../adapters/index.js';
 
@@ -48,4 +51,19 @@ export const makeGetTransactionsByUserIdController = () => {
         new GetTransactionsByUserIdController(getTransactionsByUserIdUseCase);
 
     return getTransactionsByUserIdController;
+};
+
+export const makeUpdateTransactionController = () => {
+    const updateTransactionRepository =
+        new PostgresUpdateTransactionRepository();
+
+    const updateTransactionUseCase = new UpdateTransactionUseCase(
+        updateTransactionRepository,
+    );
+
+    const updateTransactionController = new UpdateTransactionController(
+        updateTransactionUseCase,
+    );
+
+    return updateTransactionController;
 };
