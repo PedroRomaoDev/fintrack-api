e2066dab248465aa02589537252ec0296ac91725
PedroRomaoDev <pedro7ntj@gmail.com>
2025-04-19 11:50:37 -0300

    feat: add PostgresDeleteTransactionRepository

---
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -0,0 +1,16 @@
+import { prisma } from '../../../../prisma/prisma.js';
+
+export class PostgresDeleteTransactionRepository {
+    async execute(transactionId) {
+        try {
+            return await prisma.transaction.delete({
+                where: {
+                    id: transactionId,
+                },
+            });
+            // eslint-disable-next-line no-unused-vars
+        } catch (error) {
+            return null;
+        }
+    }
+}
