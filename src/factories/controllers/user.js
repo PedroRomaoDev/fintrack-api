236db0f59398e3c55fa1f4de60cb21326004776b
PedroRomaoDev <pedro7ntj@gmail.com>
2025-04-24 16:25:41 -0300

    feat: add makeGetUserBalanceController factory

--- src/factories/controllers/user.js
+++ src/factories/controllers/user.js
@@ -4,34 +4,26 @@ import {
     PostgresGetUserByEmailRepository,
     PostgresUpdateUserRepository,
     PostgresDeleteUserRepository,
+    PostgresGetUserBalanceRepository,
 } from '../../repositories/postgres/index.js';
 import {
     GetUserByIdUseCase,
     CreateUserUseCase,
     UpdateUserUseCase,
     DeleteUserUseCase,
+    GetUserBalanceUseCase,
 } from '../../use-cases/index.js';
 import {
     GetUserByIdController,
     CreateUserController,
     UpdateUserController,
     DeleteUserController,
+    GetUserBalanceController,
 } from '../../controllers/index.js';
-
-export const makeCreateUserController = () => {
-    const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
-
-    const createUserRepository = new PostgresCreateUserRepository();
-
-    const createUserUseCase = new CreateUserUseCase(
-        getUserByEmailRepository,
-        createUserRepository,
-    );
-
-    const createUserController = new CreateUserController(createUserUseCase);
-
-    return createUserController;
-};
+import {
+    IdGeneratorAdapter,
+    PasswordHasherAdapter,
+} from '../../adapters/index.js';
 
 export const makeGetUserByIdController = () => {
     const getUserByIdRepository = new PostgresGetUserByIdRepository();
@@ -43,14 +35,38 @@ export const makeGetUserByIdController = () => {
     return getUserByIdController;
 };
 
+export const makeCreateUserController = () => {
+    const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
+
+    const createUserRepository = new PostgresCreateUserRepository();
+
+    const passwordHasherAdapter = new PasswordHasherAdapter();
+
+    const idGeneratorAdapter = new IdGeneratorAdapter();
+
+    const createUserUseCase = new CreateUserUseCase(
+        getUserByEmailRepository,
+        createUserRepository,
+        passwordHasherAdapter,
+        idGeneratorAdapter,
+    );
+
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    return createUserController;
+};
+
 export const makeUpdateUserController = () => {
     const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
 
     const updateUserRepository = new PostgresUpdateUserRepository();
 
+    const passwordHasherAdapter = new PasswordHasherAdapter();
+
     const updateUserUseCase = new UpdateUserUseCase(
         getUserByEmailRepository,
         updateUserRepository,
+        passwordHasherAdapter,
     );
 
     const updateUserController = new UpdateUserController(updateUserUseCase);
@@ -66,4 +82,19 @@ export const makeDeleteUserController = () => {
     const deleteUserController = new DeleteUserController(deleteUserUseCase);
 
     return deleteUserController;
+};
+
+export const makeGetUserBalanceController = () => {
+    const getUserBalanceRepository = new PostgresGetUserBalanceRepository();
+    const getUserByIdRepository = new PostgresGetUserByIdRepository();
+
+    const getUserBalanceUseCase = new GetUserBalanceUseCase(
+        getUserBalanceRepository,
+        getUserByIdRepository,
+    );
+    const getUserBalanceController = new GetUserBalanceController(
+        getUserBalanceUseCase,
+    );
+
+    return getUserBalanceController;
 };
