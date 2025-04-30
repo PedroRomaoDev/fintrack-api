f64442127f43f8d46e2cec303f97b38d6a1ccdb0
PedroRomaoDev <pedro7ntj@gmail.com>
2025-04-30 16:10:14 -0300

    feat: ensure add UserNotFoundError to PostgresDeleteTransactionRepository for PrismaClient error 'P2025'

--- src/repositories/postgres/user/delete-user.js
+++ src/repositories/postgres/user/delete-user.js
@@ -1,4 +1,6 @@
 import { prisma } from '../../../../prisma/prisma.js';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { UserNotFoundError } from '../../../errors/index.js';
 
 export class PostgresDeleteUserRepository {
     async execute(userId) {
@@ -7,8 +9,14 @@ export class PostgresDeleteUserRepository {
                 where: { id: userId },
             });
         } catch (error) {
-            console.error(error);
-            return null;
+            if (error instanceof PrismaClientKnownRequestError) {
+                // P2025: "An operation failed because it depends on one or more records that were required but not found.
+                // (from Prisma Docs)"
+                if (error.code === 'P2025') {
+                    throw new UserNotFoundError(userId);
+                }
+            }
+            throw error;
         }
     }
 }
