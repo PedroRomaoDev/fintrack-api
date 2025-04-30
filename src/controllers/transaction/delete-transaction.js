3faa46a47d5e991d33486c6e20a6e9c92124b8fe
PedroRomaoDev <pedro7ntj@gmail.com>
2025-04-30 16:14:11 -0300

    feat: ensure verification if error is instanceof TransactionNotFoundError

--- src/controllers/transaction/delete-transaction.js
+++ src/controllers/transaction/delete-transaction.js
@@ -1,3 +1,4 @@
+import { TransactionNotFoundError } from '../../errors/transaction.js';
 import {
     checkIfIdIsValid,
     invalidIdResponse,
@@ -32,6 +33,9 @@ export class DeleteTransactionController {
 
             return ok(deletedTransaction);
         } catch (error) {
+            if (error instanceof TransactionNotFoundError) {
+                return transactionNotFoundResponse();
+            }
             console.error(error);
 
             return serverError();
