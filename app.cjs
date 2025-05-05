fed3c627fcf3b778355d4cf1526c5df7443dd9f0
PedroRomaoDev <pedro7ntj@gmail.com>
2025-05-04 21:37:11 -0300

    feat: add read swagger.json to app.cjs for swaggerUI

--- app.cjs
+++ app.cjs
@@ -1,5 +1,8 @@
 const express = require('express');
 const { usersRouter, transactionsRouter } = require('./routes/index.js');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
@@ -8,6 +11,11 @@ app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use('/api/transactions', transactionsRouter);
 
+const swaggerDocument = JSON.parse(
+    // eslint-disable-next-line no-undef
+    fs.readFileSync(path.join(__dirname, '../docs/swagger.json'), 'utf8'),
+);
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = { app };
