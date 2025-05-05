2cda22cabf645d2ba8446452128bb97fe8d544c1
PedroRomaoDev <pedro7ntj@gmail.com>
2025-05-05 16:15:35 -0300

    feat: ensure app.js organized

--- src/app.js
+++ src/app.js
@@ -4,15 +4,16 @@ import swaggerUi from 'swagger-ui-express';
 import fs from 'fs';
 import path from 'path';
 
-// ES Modules - path para usar o __dirname
-const __dirname = path.resolve(); //vai funcionar sem importar diretamente `import.meta.url`
-
 export const app = express();
 
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
 app.use('/api/transactions', transactionsRouter);
+
+//SWAGGER
+// ES Modules - path para usar o __dirname
+const __dirname = path.resolve(); //vai funcionar sem importar diretamente `import.meta.url`
 
 const swaggerDocument = JSON.parse(
     fs.readFileSync(path.join(__dirname, './docs/swagger.json'), 'utf8'),
