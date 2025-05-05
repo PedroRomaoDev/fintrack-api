895065509dbf9dd5aa826eff270b2c1c7b26cb0c
PedroRomaoDev <pedro7ntj@gmail.com>
2025-05-04 21:46:07 -0300

    feat: correct app.js (ESmodules)

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { usersRouter, transactionsRouter } from './routes/index.js';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import path from 'path';
+
+// ES Modules - path para usar o __dirname
+const __dirname = path.resolve(); //vai funcionar sem importar diretamente `import.meta.url`
+
+export const app = express();
+
+app.use(express.json());
+
+app.use('/api/users', usersRouter);
+app.use('/api/transactions', transactionsRouter);
+
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.join(__dirname, './docs/swagger.json'), 'utf8'),
+);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
