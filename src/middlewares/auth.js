2d9fc0eefc052b7ab717378e881f03fb79592467
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-12 16:30:04 -0300

    feat: add auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken';
+
+export const auth = (request, response, next) => {
+    try {
+        // pegar o access token do header
+        const accessToken = request.headers?.authorization?.split('Bearer ')[1];
+        if (!accessToken) {
+            return response.status(401).send({
+                message: 'Unauthorized',
+            });
+        }
+
+        // verificar se o access token é válido
+        const decodedToken = jwt.verify(
+            accessToken,
+            // eslint-disable-next-line no-undef
+            process.env.JWT_ACCESS_TOKEN_SECRET,
+        );
+        if (!decodedToken) {
+            return response.status(401).send({
+                message: 'Unauthorized',
+            });
+        }
+
+        request.userId = decodedToken.userId;
+
+        // se for válido, deixar a requisição prosseguir - retornar o payload
+        next();
+    } catch (error) {
+        console.error(error);
+        // se não for válido, retornar um erro 401
+        return response.status(401).send({
+            message: 'Unauthorized',
+        });
+    }
+};
