bc29c45684bc630109887e06ae62fc62060d0853
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-21 16:17:12 -0300

    feat: add LoginUserUseCase

---
+++ src/use-cases/user/login-user.js
@@ -0,0 +1,35 @@
+import { InvalidPasswordError, UserNotFoundError } from '../../errors/user.js';
+
+export class LoginUserUseCase {
+    constructor(
+        getUserByEmailRepository,
+        passwordComparatorAdapter,
+        tokenGeneratorAdapter,
+    ) {
+        this.getUserByEmailRepository = getUserByEmailRepository;
+        this.passwordComparatorAdapter = passwordComparatorAdapter;
+        this.tokenGeneratorAdapter = tokenGeneratorAdapter;
+    }
+    async execute(email, password) {
+        // verificaremos se o e-mail é válido (se há usuário com esse e-mail)
+        const user = await this.getUserByEmailRepository.execute(email);
+        if (!user) {
+            throw new UserNotFoundError();
+        }
+
+        // verificaremos se a senha recebida é válida (se a senha está correta)
+        const isPasswordValid = await this.passwordComparatorAdapter.execute(
+            password,
+            user.password,
+        );
+        if (!isPasswordValid) {
+            throw new InvalidPasswordError();
+        }
+
+        // depois, gerar os tokens
+        return {
+            ...user,
+            tokens: this.tokenGeneratorAdapter.execute(user.id),
+        };
+    }
+}
