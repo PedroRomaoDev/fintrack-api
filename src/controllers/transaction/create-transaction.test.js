926d92c8b72a00914c49ddc39312feaf0e9d6535
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-25 00:24:30 -0300

    test: ensure CreateTransactionController return 201 when creating transaction succesfully {expense}

---
+++ src/controllers/transaction/create-transaction.test.js
@@ -0,0 +1,38 @@
+import { CreateTransactionController } from './create-transaction.js';
+import { transaction } from '../../tests/index.js';
+
+describe('CreateTransactionController', () => {
+    class CreateTransactionUseCaseStub {
+        async execute() {
+            return transaction;
+        }
+    }
+
+    const makeSut = () => {
+        const createTransactionUseCase = new CreateTransactionUseCaseStub();
+        const sut = new CreateTransactionController(createTransactionUseCase);
+
+        return {
+            sut,
+            createTransactionUseCase,
+        };
+    };
+
+    const baseHttpRequest = {
+        body: {
+            ...transaction,
+            id: undefined,
+        },
+    };
+
+    it('should return 201 when creating transaction succesfully {expense}', async () => {
+        // arrange
+        const { sut } = makeSut();
+
+        // act
+        const result = await sut.execute(baseHttpRequest);
+
+        // assert
+        expect(result.statusCode).toBe(201);
+    });
+});
