38e19164680c080d77333651526b967d4ac04335
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-25 00:57:01 -0300

    feat: updates GetTransactionsByUserIdController

--- src/controllers/transaction/get-transactions-by-user-id.js
+++ src/controllers/transaction/get-transactions-by-user-id.js
@@ -1,11 +1,11 @@
+import { ZodError } from 'zod';
 import { UserNotFoundError } from '../../errors/user.js';
+import { getTransactionsByUserIdSchema } from '../../schemas/transaction.js';
 import {
-    checkIfIdIsValid,
-    invalidIdResponse,
-    requiredFieldIsMissingResponse,
     serverError,
     userNotFoundResponse,
     ok,
+    badRequest,
 } from '../helpers/index.js';
 
 export class GetTransactionsByUserIdController {
@@ -15,25 +15,31 @@ export class GetTransactionsByUserIdController {
     async execute(httpRequest) {
         try {
             const userId = httpRequest.query.userId;
+            const from = httpRequest.query.from;
+            const to = httpRequest.query.to;
             //verificar se o userid foi passado como parametro
-            if (!userId) {
-                return requiredFieldIsMissingResponse('userId');
-            }
-
-            //veriricar se o userId é um ID válido
-            const userIdIsValid = checkIfIdIsValid(userId);
-
-            if (!userIdIsValid) {
-                return invalidIdResponse();
-            }
+            await getTransactionsByUserIdSchema.parseAsync({
+                user_id: userId,
+                from,
+                to,
+            });
 
             //chamar o useCase
             const transactions =
-                await this.getTransactionsByUserIdUseCase.execute(userId);
+                await this.getTransactionsByUserIdUseCase.execute(
+                    userId,
+                    from,
+                    to,
+                );
 
             //retornar resposta http
             return ok(transactions);
         } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                });
+            }
             if (error instanceof UserNotFoundError) {
                 return userNotFoundResponse();
             }
