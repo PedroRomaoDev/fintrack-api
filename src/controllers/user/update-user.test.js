9b677c50bd67fd2d58e673aaa0eda3f7743e1c53
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-24 22:51:45 -0300

    test: ensure UpdateUserController returns 400 when invalid password is provided

--- src/controllers/user/update-user.test.js
+++ src/controllers/user/update-user.test.js
@@ -60,4 +60,22 @@ describe('UpdateUserController', () => {
         // assert
         expect(result.statusCode).toBe(400);
     });
+    it('should return 400 when invalid password is provided', async () => {
+        // arrange
+        const { sut } = makeSut();
+
+        // act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: {
+                ...httpRequest,
+                password: faker.internet.password({
+                    length: 5,
+                }),
+            },
+        });
+
+        // assert
+        expect(result.statusCode).toBe(400);
+    });
 });
