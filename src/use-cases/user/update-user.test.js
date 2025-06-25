db9c6104b8453ad0635d6c617f555a881a7cb1f4
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-24 23:48:43 -0300

    test: ensure UpdateUserUseCase throws UpdateUserRepository throws

--- src/use-cases/user/update-user.test.js
+++ src/use-cases/user/update-user.test.js
@@ -151,4 +151,22 @@ describe('UpdateUserUseCase', () => {
         // assert
         await expect(promise).rejects.toThrow();
     });
+    it('should throw UpdateUserRepository throws', async () => {
+        // arrange
+        const { sut, updateUserRepository } = makeSut();
+        jest.spyOn(updateUserRepository, 'execute').mockRejectedValue(
+            new Error(),
+        );
+
+        // act
+        const promise = sut.execute(faker.string.uuid(), {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        });
+
+        // assert
+        await expect(promise).rejects.toThrow();
+    });
 });
