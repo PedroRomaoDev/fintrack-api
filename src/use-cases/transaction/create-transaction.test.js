8ee950e04d12acc787d18e48645a5fd321f3a934
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-24 23:54:11 -0300

    test: ensure CreateTransactionUseCase calls CreateTransactionRepository with correct params

--- src/use-cases/transaction/create-transaction.test.js
+++ src/use-cases/transaction/create-transaction.test.js
@@ -83,4 +83,21 @@ describe('CreateTransactionUseCase', () => {
         // assert
         expect(idGeneratorAdapterSpy).toHaveBeenCalled();
     });
+    it('should call CreateTransactionRepository with correct params', async () => {
+        // arrange
+        const { sut, createTransactionRepository } = makeSut();
+        const createTransactionRepositorySpy = jest.spyOn(
+            createTransactionRepository,
+            'execute',
+        );
+
+        // act
+        await sut.execute(createTransactionParams);
+
+        // assert
+        expect(createTransactionRepositorySpy).toHaveBeenCalledWith({
+            ...createTransactionParams,
+            id: 'random_id',
+        });
+    });
 });
