f7a1904898f4f4c103245ec9ef4baed3bfc1de65
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-25 00:28:14 -0300

    test: ensure CreateTransactionController return 400 when type is not EXPENSE, EARNING or INVESTMENT

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -155,4 +155,19 @@ describe('CreateTransactionController', () => {
         // assert
         expect(result.statusCode).toBe(400);
     });
+    it('should return 400 when type is not EXPENSE, EARNING or INVESTMENT', async () => {
+        // arrange
+        const { sut } = makeSut();
+
+        // act
+        const result = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                type: 'invalid_type',
+            },
+        });
+
+        // assert
+        expect(result.statusCode).toBe(400);
+    });
 });
