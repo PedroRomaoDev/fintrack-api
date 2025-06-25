393bbe4af1d3974445762d8fbc5fc48bcf311da0
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-24 23:46:21 -0300

    test: ensure UpdateUserUseCase update user succesfully (with password)

--- src/use-cases/user/update-user.test.js
+++ src/use-cases/user/update-user.test.js
@@ -67,4 +67,20 @@ describe('UpdateUserUseCase', () => {
         expect(getUserByEmailRepositorySpy).toHaveBeenCalledWith(email);
         expect(result).toBe(user);
     });
+    it('should update user succesfully (with password)', async () => {
+        // arrange
+        const { sut, passwordHasherAdapter } = makeSut();
+        const passwordHasherAdapterSpy = jest.spyOn(
+            passwordHasherAdapter,
+            'execute',
+        );
+        const password = faker.internet.password();
+
+        // act
+        const result = await sut.execute(faker.string.uuid(), { password });
+
+        // assert
+        expect(passwordHasherAdapterSpy).toHaveBeenCalledWith(password);
+        expect(result).toBe(user);
+    });
 });
