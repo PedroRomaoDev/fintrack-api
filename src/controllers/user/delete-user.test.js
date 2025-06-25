8feec87650c4fbcfd526a5d08b8da20a7d5ca1f9
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-24 22:20:12 -0300

    test: DeleteUserController returns 500 if DeleteUserUseCase throws

--- src/controllers/user/delete-user.test.js
+++ src/controllers/user/delete-user.test.js
@@ -56,4 +56,15 @@ describe('DeleteUserController', () => {
         // assert
         expect(result.statusCode).toBe(404);
     });
+    it('should return 500 if DeleteUserUseCase throws', async () => {
+        // arrange
+        const { sut, deleteUserUseCase } = makeSut();
+        jest.spyOn(deleteUserUseCase, 'execute').mockRejectedValue(new Error());
+
+        // act
+        const result = await sut.execute(httpRequest);
+
+        // assert
+        expect(result.statusCode).toBe(500);
+    });
 });
