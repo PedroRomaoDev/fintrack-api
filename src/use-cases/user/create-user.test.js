f9729362de4109155939693b33acc275087154f6
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-24 23:12:15 -0300

    test: ensure CreateUserUseCase throws if CreateUserRepository throws

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -184,4 +184,20 @@ describe('CreateUserUseCase', () => {
         // assert
         await expect(promise).rejects.toThrow();
     });
+
+    it('should throw if CreateUserRepository throws', async () => {
+        // arrange
+        const { sut, createUserRepository } = makeSut();
+        jest.spyOn(createUserRepository, 'execute').mockImplementationOnce(
+            () => {
+                throw new Error();
+            },
+        );
+
+        // act
+        const promise = sut.execute(user);
+
+        // assert
+        await expect(promise).rejects.toThrow();
+    });
 });
