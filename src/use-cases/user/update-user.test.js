168d9b128edd30776dbfe69720e8f37fc1eb3e36
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-24 23:46:04 -0300

    test: ensure UpdateUserUseCase update user succesfully (with email)

--- src/use-cases/user/update-user.test.js
+++ src/use-cases/user/update-user.test.js
@@ -51,4 +51,20 @@ describe('UpdateUserUseCase', () => {
         // assert
         expect(result).toBe(user);
     });
+    it('should update user succesfully (with email)', async () => {
+        // arrange
+        const { sut, getUserByEmailRepository } = makeSut();
+        const getUserByEmailRepositorySpy = jest.spyOn(
+            getUserByEmailRepository,
+            'execute',
+        );
+        const email = faker.internet.email();
+
+        // act
+        const result = await sut.execute(faker.string.uuid(), { email });
+
+        // assert
+        expect(getUserByEmailRepositorySpy).toHaveBeenCalledWith(email);
+        expect(result).toBe(user);
+    });
 });
