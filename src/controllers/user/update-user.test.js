b6f0768098e7c43c5b9eb82b2a6d88490e99ce0d
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-24 22:48:29 -0300

    test: ensure UpdateUserController returns 200 when updating a user succesfully

---
+++ src/controllers/user/update-user.test.js
@@ -0,0 +1,47 @@
+import { faker } from '@faker-js/faker';
+import { UpdateUserController } from './update-user.js';
+// import {
+//     EmailAlreadyInUseError,
+//     UserNotFoundError,
+// } from '../../errors/user.js';
+import { user } from '../../tests/index.js';
+
+describe('UpdateUserController', () => {
+    class UpdateUserUseCaseStub {
+        async execute() {
+            return user;
+        }
+    }
+
+    const makeSut = () => {
+        //Sut = suits under test
+        const updateUserUseCase = new UpdateUserUseCaseStub();
+        const sut = new UpdateUserController(updateUserUseCase);
+        return { updateUserUseCase, sut };
+    };
+
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+        body: {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password({
+                length: 7,
+            }),
+        },
+    };
+
+    it('should return 200 when updating a user succesfully', async () => {
+        // arrange
+        const { sut } = makeSut();
+
+        // act
+        const result = await sut.execute(httpRequest);
+
+        // assert
+        expect(result.statusCode).toBe(200);
+    });
+});
